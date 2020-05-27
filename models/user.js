9418a652da9b691e54283d1ddc66f266b9d12bc9
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-05-27 03:24:45 +0200

    create model for users

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const sequelize = require("sequelize");
+const db = require("../config/database");
+
+const User = db.define("user", {
+  id: {
+    type: sequelize.String,
+    primaryKey: true,
+  },
+  username: {
+    type: sequelize.String,
+  },
+});
+
+module.exports = User;
